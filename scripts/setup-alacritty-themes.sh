712e541747f1213faac12b9b9b10463e87d3b12b
Marcus441 <psminchia4153@hotmail.com>
2024-08-10 22:08:55 +1000

    Fixed alacritty setup script

--- scripts/setup-alacritty-themes.sh
+++ scripts/setup-alacritty-themes.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # Variables
-THEME_DIR="$HOME/.config/alacritty/"
+THEME_DIR="$HOME/.config/alacritty/themes"
 REPO_URL="https://github.com/alacritty/alacritty-theme"
 CONFIG_FILE="$HOME/.config/alacritty/alacritty.yml"
 
-# Create the theme directory if it doesn't exist
-mkdir -p "$THEME_DIR/themes"
-
 # Clone the themes repository if it doesn't exist
-if [ ! -d "$THEME_DIR/themes" ]; then
-  echo "Cloning themes repository..."
-  git clone "$REPO_URL" "$THEME_DIR/themes"
-else
+# Check if directory is empty
+if ["$ls -A "$THEME_DIR")"]; then
   echo "Themes repository already exists."
-  cd "$THEME_DIR/themes" && git pull
+  cd "$THEME_DIR" && git pull
+else
+  echo "Cloning themes repository..."
+  git clone "$REPO_URL" "$THEME_DIR"
 fi
