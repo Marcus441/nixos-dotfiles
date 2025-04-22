97020f96a9c1e702c0001b0210c9359b3a1967ae
Marcus441 <psminchia4153@hotmail.com>
2025-04-22 17:46:39 +1000

    feat: add more config for alacritty

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,7 +1,24 @@
-{ lib, ... }: {
+{ config, lib, ... }:
+
+
+{
+# Fetch the theme repo into your config
+  home.file.".config/alacritty/themes".source = 
+    builtins.fetchGit {
+      url = "https://github.com/alacritty/alacritty-theme.git";
+      rev = "0520b1f8f3eb25678444c0817a97636b6601ac85"; # You can pin a specific commit for stability
+      allRefs = true;
+    };
+
+
   programs.alacritty = {
     enable = true;
     settings = {
+
+      general.import = [
+        "${config.home.homeDirectory}/.config/alacritty/themes/themes/gruvbox_material_hard_dark.toml"
+      ];
+
       window = {
 	 padding.x = 15;
 	 padding.y = 15;
@@ -16,6 +33,18 @@
       terminal.shell = {
         program = "zsh";
       };
+
+      cursor.style = {
+        shape = "Block";
+        blinking = "Always";
+      };
+
+      selection.save_to_clipboard = true;
+
+      env = {
+        TERM = "xterm-256color";
+      };
     };
   };
 }
+
