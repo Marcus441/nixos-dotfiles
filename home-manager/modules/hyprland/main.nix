520fba15791abb1a16acff8d710dcd88cfdb7de4
Marcus441 <psminchia4153@hotmail.com>
2025-04-24 13:13:38 +1000

    style: added window animations

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -64,7 +64,29 @@ in
       };
 
       animations = {
-        enabled = false;
+        enabled = true;
+
+        bezier = [
+          "linear, 0, 0, 1, 1"
+          "md3_standard, 0.2, 0, 0, 1"
+          "md3_decel, 0.05, 0.7, 0.1, 1"
+          "md3_accel, 0.3, 0, 0.8, 0.15"
+          "overshot, 0.05, 0.9, 0.1, 1.1"
+          "crazyshot, 0.1, 1.5, 0.76, 0.92"
+          "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+          "fluent_decel, 0.1, 1, 0, 1"
+          "easeInOutCirc, 0.85, 0, 0.15, 1"
+          "easeOutCirc, 0, 0.55, 0.45, 1"
+          "easeOutExpo, 0.16, 1, 0.3, 1"
+        ];
+
+        animation = [
+          "windows, 1, 3, md3_decel, popin 60%"
+          "border, 1, 10, default"
+          "fade, 1, 2.5, md3_decel"
+          "workspaces, 1, 3.5, easeOutExpo, slide"
+          "specialWorkspace, 1, 3, md3_decel, slidevert"
+        ];
       };
 
       input = {
@@ -105,10 +127,8 @@ in
 
         "workspace 1,class:(chromium)"
         "workspace 2,class:(terminal)"
-        "workspace 3,class:(discord)"
-        "workspace 4,class:(mail)"
-        "workspace 5,class:(file)"
-        "workspace 6,class:(teams-for-linux)"
+        "workspace 3,class:(vesktop)"
+        "workspace 8,class:(teams-for-linux)"
 
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
