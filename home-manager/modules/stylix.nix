5ad92afd79252c2bb4bf1534f620ece43814e156
Marcus441 <psminchia4153@hotmail.com>
2025-05-04 15:16:57 +1000

    changed wallpaper

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,4 +1,4 @@
-{config,lib, pkgs, inputs, ... }: {
+{ config, lib, pkgs, inputs, ... }: {
   imports = [ inputs.stylix.homeManagerModules.stylix ];
 
   home.packages = with pkgs; [
@@ -17,9 +17,9 @@
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
     override = {
-       base00 = "#1d2021";  # Correct the base00 color (background)
-       # You can override other colors as needed, e.g., base01, base08, etc.
-    };    
+      base00 = "#1d2021"; # Correct the base00 color (background)
+      # You can override other colors as needed, e.g., base01, base08, etc.
+    };
     targets = {
       neovim.enable = false;
       waybar.enable = false;
@@ -66,8 +66,8 @@
     };
 
     image = pkgs.fetchurl {
-      url = "https://codeberg.org/AXP-OS/wallpapers/raw/branch/main/RAW/alex-holzreiter-408703.jpg";
-      sha256 = "12e026e92d2f5e3634332fc94678b9d2f57826e3cb83f852982150052d121b5a";
+      url = "https://codeberg.org/AXP-OS/wallpapers/raw/branch/main/RAW/joshua-sortino-274493.jpg";
+      sha256 = "f209775f7d398f7a828c7b51e66a37b64fb5d6f2ecaff513d6ee39eddad37a37";
     };
   };
 }
