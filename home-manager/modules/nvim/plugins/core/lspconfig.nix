9955c6fb694996adfba233fb495af8757281212a
Marcus441 <psminchia4153@hotmail.com>
2025-06-17 18:33:11 +1000

    updated nvim config

--- home-manager/modules/nvim/plugins/core/lspconfig.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-    vim.lsp.lspconfig.enable = true;
-
-    vim.lsp.lspconfig.sources = {
-      # python = "pyright"; 
-      # typescript = "tsserver";
-    };
-
-}
--- home-manager/modules/nvim/plugins/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [
-    ./core/lspocnfig.nix
-    ./core/nvim-cmp.nix
-    ./core/colorscheme.nix
-    # ./core/treesitter.nix
-   ];
-}
