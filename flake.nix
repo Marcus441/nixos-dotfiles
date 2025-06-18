f5feab326d7da306193825722c48c1603299dd12
Marcus441 <psminchia4153@hotmail.com>
2025-06-18 17:48:30 +1000

    chore(flake): added nvf home manager module

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
