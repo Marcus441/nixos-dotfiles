4f119f0c60587b7ab76c289dce0e0023d17a5b59
Marcus441 <psminchia4153@hotmail.com>
2025-06-19 12:51:15 +1000

    feat(nvim): added notification autocmd for macro

--- home-manager/modules/nvf.nix
+++ home-manager/modules/nvf.nix
@@ -12,7 +12,14 @@
 
     settings.vim = {
       lsp.enable = true;
-      augroups = [{name = "UserSetup";}];
+      augroups = [
+        {
+          name = "UserSetup";
+        }
+        {
+          name = "MacroRecordingNotificationGroup";
+        }
+      ];
       autocmds = [
         {
           event = ["TextYankPost"];
@@ -21,6 +28,36 @@
           callback = lib.mkLuaInline ''
             function()
               vim.highlight.on_yank()
+            end
+          '';
+        }
+        {
+          event = ["RecordingEnter"];
+          desc = "Notify when macro recording starts";
+          group = "MacroRecordingNotificationGroup";
+          callback = lib.mkLuaInline ''
+            function()
+              local msg = string.format("Register: %s", vim.fn.reg_recording())
+              _MACRO_RECORDING_STATUS = true
+              vim.notify(msg, vim.log.levels.INFO, {
+                title = "Macro Recording",
+                -- The `keep` function is embedded directly within the Lua string
+                keep = function() return _MACRO_RECORDING_STATUS end,
+              })
+            end
+          '';
+        }
+        {
+          event = ["RecordingLeave"];
+          desc = "Notify when macro recording ends";
+          group = "MacroRecordingNotificationGroup";
+          callback = lib.mkLuaInline ''
+            function()
+              _MACRO_RECORDING_STATUS = false
+              vim.notify("Success!", vim.log.levels.INFO, {
+                title = "Macro Recording End",
+                timeout = 2000,
+              })
             end
           '';
         }
