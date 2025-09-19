2574fc37f42f3c6dde45af9906b5ce5f96c8f5f0
Marcus441 <psminchia4153@hotmail.com>
2025-09-19 18:14:25 +1000

    style(starship): stripped redundant styling

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -1,7 +1,6 @@
 {config, ...}: let
   accent = "#${config.lib.stylix.colors.base0E}";
   red = "#${config.lib.stylix.colors.base08}";
-  green = "#${config.lib.stylix.colors.base0B}";
 in {
   programs.starship = {
     enable = true;
@@ -9,27 +8,13 @@ in {
     settings = {
       add_newline = true;
 
-      hostname = {
-        ssh_only = false;
-        format = "[$ssh_symbol$hostname]($style) ";
-        style = "bold ${green}";
-      };
-
       character = {
         success_symbol = "[➜](bold ${accent})";
         error_symbol = "[✘](bold ${red})";
       };
 
-      username = {
-        show_always = true;
-        format = "[$user]($style)@";
-        style_user = "bold ${accent}";
-      };
-
       directory = {
         read_only = " 🔒";
-        truncation_symbol = "…/";
-        style = "${green}";
       };
     };
   };
