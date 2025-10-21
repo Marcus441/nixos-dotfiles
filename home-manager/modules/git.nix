dc8fa27b2bb0d5ba0ddd48d5c3109b3c5b690875
Marcus441 <psminchia4153@hotmail.com>
2025-10-21 22:18:46 +1000

    chore(update): update system and git config
    - Use up to date git config
    - Update system

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,17 +1,11 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
   programs.git = {
     enable = true;
-    userName = "Marcus441";
-    userEmail = "psminchia4153@hotmail.com";
-    extraConfig = {
-      # Set Git to use SSH instead of HTTPS for GitHub URLs
+    settings = {
+      user.name = "Marcus441";
+      user.email = "psminchia4153@hotmail.com";
       "url.git@github.com:".insteadOf = "https://github.com/";
-      # Ensure submodules use SSH as well
-      submodule.recurse = "true";
+      submodule.recurse = true;
       submodule.url = "git@github.com:";
       core.sshCommand = "ssh -i /home/marcus/.ssh/id_ed25519";
     };
