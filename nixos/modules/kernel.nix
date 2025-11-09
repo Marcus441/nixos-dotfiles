48b99a766772620f2113daa0c377e7dd13736d78
Marcus441 <psminchia4153@hotmail.com>
2025-11-09 19:19:53 +1000

    feat(rtl885bu): added support for driver

--- nixos/modules/kernel.nix
+++ nixos/modules/kernel.nix
@@ -1,3 +1,16 @@
-{pkgs, ...}: {
-  boot.kernelPackages = pkgs.linuxPackages;
+{
+  pkgs,
+  config,
+  ...
+}: {
+  boot = {
+    kernelPackages = pkgs.linuxPackages;
+    extraModulePackages = with config.boot.kernelPackages; [
+      rtl8852bu
+    ];
+    modprobeConfig.enable = true;
+    extraModprobeConfig = ''
+      options 8852bu rtw_switch_usb_mode=1 rtw_he_enable=1 rtw_vht_enable=1
+    '';
+  };
 }
