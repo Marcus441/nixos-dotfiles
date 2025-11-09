915e3421bf81e9275f21c216249c52172350924b
Marcus441 <psminchia4153@hotmail.com>
2025-11-09 19:29:57 +1000

    chore(neovim): added bind to open minifiles in cwd

--- home-manager/modules/neovim/keymaps/mini-files.nix
+++ home-manager/modules/neovim/keymaps/mini-files.nix
@@ -2,6 +2,12 @@
   {
     mode = ["n"];
     key = "<leader>e";
+    desc = "Toggle MiniFiles in CWD";
+    action = "<CMD>lua local mf = require('mini.files'); if not mf.close() then mf.open(vim.api.nvim_buf_get_name(0), false) end; vim.schedule(function() mf.reveal_cwd() end)<CR>";
+  }
+  {
+    mode = ["n"];
+    key = "<leader>E";
     desc = "Toggle MiniFiles";
     action = "<CMD>lua if not MiniFiles.close() then MiniFiles.open() end <CR>";
   }
