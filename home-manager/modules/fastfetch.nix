a409c978d957dd51c49c41d3d6c98d528a0d1c8c
Marcus441 <psminchia4153@hotmail.com>
2025-11-14 20:24:27 +1000

    feat(fastfetch): added fastfetch styling

---
+++ home-manager/modules/fastfetch.nix
@@ -0,0 +1,182 @@
+{
+  programs.fastfetch = {
+    enable = true;
+
+    settings = {
+      logo = {
+        padding = {
+          top = 1;
+        };
+      };
+
+      display = {separator = " -> ";};
+
+      modules = [
+        "break"
+
+        {
+          type = "custom";
+          format = "╭────────────────────── Hardware ──────────────────────╮";
+          outputColor = "red";
+        }
+
+        {
+          type = "title";
+          key = " PC";
+          keyColor = "green";
+          format = "{user-name-colored} on {host-name-colored}";
+        }
+
+        {
+          type = "cpu";
+          key = "│ ├󰍛 CPU";
+          showPeCoreCount = true;
+          format = "{name} {freq-max}";
+          keyColor = "green";
+        }
+
+        {
+          type = "gpu";
+          key = "│ ├󰍛 GPU";
+          keyColor = "green";
+          format = "{vendor} {name}";
+        }
+
+        {
+          type = "disk";
+          key = "│ ├󱛟 Disk";
+          keyColor = "green";
+        }
+
+        {
+          type = "memory";
+          key = "└ └󰍛 Memory";
+          keyColor = "green";
+        }
+
+        {
+          type = "custom";
+          format = "╰──────────────────────────────────────────────────────╯";
+          outputColor = "red";
+        }
+
+        "break"
+
+        {
+          type = "custom";
+          format = "╭────────────────────── Software ──────────────────────╮";
+          outputColor = "red";
+        }
+
+        {
+          type = "os";
+          key = " OS";
+          keyColor = "yellow";
+          format = "NixOS {version-id} {codename}";
+        }
+
+        {
+          type = "kernel";
+          key = "│ ├ Kernel";
+          keyColor = "yellow";
+        }
+
+        {
+          type = "shell";
+          key = "│ ├ Shell";
+          keyColor = "yellow";
+          format = "{1}";
+        }
+
+        {
+          type = "packages";
+          key = "│ ├󰏖 Packages";
+          keyColor = "yellow";
+        }
+
+        {
+          type = "command";
+          key = "│ ├ OS Age";
+          keyColor = "yellow";
+          text = ''
+            birth_install=$(stat -c %W /); \
+            current=$(date +%s); \
+            time_progression=$((current - birth_install)); \
+            days_difference=$((time_progression / 86400)); \
+            echo $days_difference days
+          '';
+        }
+
+        {
+          type = "uptime";
+          key = "└ └ Uptime";
+          keyColor = "yellow";
+        }
+
+        "break"
+
+        {
+          type = "de";
+          key = " DE";
+          keyColor = "blue";
+          format = "{1}";
+        }
+
+        {
+          type = "wm";
+          key = "│ ├ Compositor";
+          keyColor = "blue";
+          format = "{1}";
+        }
+
+        {
+          type = "lm";
+          key = "│ ├ Login";
+          keyColor = "blue";
+          format = "{1}";
+        }
+
+        {
+          type = "terminal";
+          key = "│ ├ Terminal";
+          keyColor = "blue";
+          format = "{1}";
+        }
+
+        {
+          type = "terminalfont";
+          key = "│ ├ Font";
+          keyColor = "blue";
+          format = "{name}";
+        }
+
+        {
+          type = "icons";
+          key = "│ ├ Icons";
+          keyColor = "blue";
+        }
+
+        {
+          type = "custom";
+          key = "└ └ Theme";
+          keyColor = "blue";
+          format = "Kanagawa Dragon";
+        }
+
+        {
+          type = "custom";
+          format = "╰──────────────────────────────────────────────────────╯";
+          outputColor = "red";
+        }
+
+        {
+          type = "colors";
+          paddingLeft = 20;
+          symbol = "circle";
+        }
+
+        "break"
+      ];
+    };
+  };
+}
