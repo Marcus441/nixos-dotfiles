b198ee6cbf95a57ab00d997cb40912eede04031f
Marcus441 <psminchia4153@hotmail.com>
2025-12-11 10:17:03 +1000

    fix(wifi): remove out of tree wifi driver
    - Now uses in tree realtek drivers and default modprobe settings

--- nixos/modules/kernel.nix
+++ nixos/modules/kernel.nix
@@ -1,16 +1,5 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
-    extraModulePackages = with config.boot.kernelPackages; [
-      rtl8852bu
-    ];
-    modprobeConfig.enable = true;
-    extraModprobeConfig = ''
-      options 8852bu rtw_switch_usb_mode=1 rtw_he_enable=1 rtw_vht_enable=1
-    '';
   };
 }
