6eb9177652c1434f9be19e131e7bb416f73edda5
Marcus441 <psminchia4153@hotmail.com>
2025-12-19 09:19:32 +1000

    chore(neovim): disabled assistant

--- home-manager/modules/neovim/assistant.nix
+++ home-manager/modules/neovim/assistant.nix
@@ -1,32 +1,22 @@
 {lib, ...}: {
-  programs.nvf.settings.vim.assistant = {
-    avante-nvim = {
-      enable = false;
-      setupOpts = {
-        behaviour.auto_suggestions = true;
-        auto_suggestions_provider = "ollama";
+  programs.nvf.settings.vim.assistant.avante-nvim = {
+    enable = false;
+    setupOpts = {
+      provider = "ollama";
+      auto_suggestions_provider = "ollama";
 
-        suggestion = {
-          debounce = 400;
-          throttle = 400;
-        };
+      behaviour = {
+        auto_suggestions = true;
+        auto_set_highlight_group = true;
+      };
 
-        # Providers
-        providers = {
-          ollama = {
-            endpoint = "http://192.168.55.2:8000";
-
-            model = "qwen2.5-Coder-1.5b-instruct-GGUF";
-            timeout = 15000;
-            is_env_set = lib.mkLuaInline ''require(" avante.providers.ollama ").check_endpoint_alive'';
-            extra_request_body = {
-              options = {
-                temperature = 0.0;
-                num_ctx = 4096;
-                num_predict = 64;
-              };
-            };
-          };
+      providers = {
+        ollama = {
+          endpoint = "http://192.168.55.2:11434/"; # Added /v1
+          model = "qwen2.5-coder:0.5b"; # Use the tag name from 'ollama list'
+          timeout = 15000;
+          # parse_curl_args = ""; # Optional: helper for nix
+          # parse_stream_data = ""; # Optional: helper for nix
         };
       };
     };
