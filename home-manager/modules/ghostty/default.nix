9bd6bd07cbe8cb5f542358723370ac7f42f73d5c
Marcus441 <psminchia4153@hotmail.com>
2025-12-19 17:47:21 +1000

    feat(ghostty): Added neovim based kanagaw dragon theme

--- home-manager/modules/ghostty/default.nix
+++ home-manager/modules/ghostty/default.nix
@@ -1,6 +1,33 @@
 {
   programs.ghostty = {
     enable = true;
+    themes = {
+      kanagawa-dragon-nvim = {
+        background = "#181616";
+        cursor-color = "#c8c093";
+        foreground = "#c5c9c5";
+        palette = [
+          "0=#0d0c0c"
+          "1=#c4746e"
+          "2=#8a9a7b"
+          "3=#c4b28a"
+          "4=#8ba4b0"
+          "5=#a292a3"
+          "6=#8ea4a2"
+          "7=#c8c093"
+          "8=#a6a69c"
+          "9=#e46876"
+          "10=#87a987"
+          "11=#e6c384"
+          "12=#7fb4ca"
+          "13=#938aa9"
+          "14=#7aa89f"
+          "15=#c5c9c5"
+        ];
+        selection-background = "#2d4f67";
+        selection-foreground = "#c8c093";
+      };
+    };
     settings = {
       # Window padding
       window-padding-x = 10;
@@ -10,6 +37,7 @@
       confirm-close-surface = false;
       custom-shader = "/home/marcus/flake/home-manager/modules/ghostty/cursor_smear.glsl";
 
+      theme = "kanagawa-dragon-nvim";
       # Font
       font-feature = "builtin_box_drawing";
 
