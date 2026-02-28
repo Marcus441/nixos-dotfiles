4fd38848a84bf2b4dcf6d05b47f4659fc4297fc9
Marcus441 <psminchia4153@hotmail.com>
2026-03-01 09:43:55 +1000

    fix(btop): fixed theming

--- home-manager/modules/btop.nix
+++ home-manager/modules/btop.nix
@@ -6,18 +6,19 @@
       cudaSupport = true;
     };
     settings = {
-      vim_keys = true;
-      rounded_corners = false;
-      update_ms = 1000;
+      cpu_sensor = "auto";
+      io_graph_combined = false;
+      io_mode = true;
+      only_physical = true;
       proc_tree = true;
+      rounded_corners = true;
+      show_coretemp = true;
+      show_disks = true;
       show_gpu_info = "on";
       show_uptime = true;
-      show_coretemp = true;
-      cpu_sensor = "auto";
-      show_disks = true;
-      only_physical = true;
-      io_mode = true;
-      io_graph_combined = false;
+      theme_background = false;
+      update_ms = 1000;
+      vim_keys = true;
     };
   };
 }
