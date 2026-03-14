7d4f54da29c35bc38de4fb72bc14aa569384319a
Marcus441 <psminchia4153@hotmail.com>
2026-03-14 14:24:17 +1000

    fix(thunar): update plugins attribute to use pkgs scope

--- nixos/modules/thunar.nix
+++ nixos/modules/thunar.nix
@@ -3,7 +3,7 @@
     xfconf.enable = true;
     thunar = {
       enable = true;
-      plugins = with pkgs.xfce; [
+      plugins = with pkgs; [
         thunar-archive-plugin
         thunar-volman
       ];
