77c41dc02c8bfde7aae804b96ca947c8380369e8
Marcus441 <psminchia4153@hotmail.com>
2026-04-04 13:45:56 +1000

    perf(nix): add gc configs

--- nixos/modules/nh.nix
+++ nixos/modules/nh.nix
@@ -2,5 +2,9 @@
   programs.nh = {
     enable = true;
     flake = "/home/${user}/flake";
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 4d --keep 3";
+    };
   };
 }
--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,3 +1,7 @@
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+    warn-dirty = false;
+  };
 }
