a1dc58149659570f6ed15708898aa4e4f5c3949f
Marcus441 <psminchia4153@hotmail.com>
2026-04-04 13:31:15 +1000

    fix(mako): use POSIX regex

--- home-manager/modules/services/mako.nix
+++ home-manager/modules/services/mako.nix
@@ -2,15 +2,6 @@
   services.mako = {
     enable = true;
 
-    extraConfig = ''
-      [app-name=notify-send summary~="OCR*"]
-      default-timeout=3000
-
-      [summary~="*Battery*"]
-      default-timeout=20000
-
-      [summary~="*screenshot*"]
-    '';
     settings = {
       anchor = "top-right";
       default-timeout = 5000;
@@ -22,5 +13,15 @@
       width = 420;
       height = 110;
     };
+    extraConfig = ''
+      [app-name=notify-send summary="OCR*"]
+      default-timeout=3000
+
+      [summary="*Battery*"]
+      default-timeout=20000
+
+      [summary="*screenshot*"]
+      default-timeout=5000
+    '';
   };
 }
