baf9833074a95fd92f2af3a117f4074a34376101
Marcus441 <psminchia4153@hotmail.com>
2026-04-04 13:46:08 +1000

    perf(boot): add settings for quicker boot times

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,6 +1,25 @@
 {
   boot = {
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10;
+      };
+      # WARNING: Hold space bar to open boot menu for generations
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+    };
+
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 }
