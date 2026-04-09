ad735b6409999a9d57da4fc2b9ad5bf8c1fe8bff
Marcus441 <psminchia4153@hotmail.com>
2026-04-10 09:15:22 +1000

    feat(btop): use base16 version of theme

--- home-manager/modules/btop.nix
+++ home-manager/modules/btop.nix
@@ -3,22 +3,25 @@
   config,
   ...
 }: let
-  dragonBg = "181616"; # dragonBlack3
-  dragonFg = "c5c9c5"; # dragonWhite
-  dragonGray = "7a8382"; # dragonGray3
-  dragonSelBg = "282727"; # dragonBlack4
-  dragonSelFg = "c4b28a"; # dragonYellow
-
-  dragonRed = "c4746e";
-  dragonOrange = "b6927b";
-  dragonYellow = "c4b28a";
-  dragonGreen = "87a987";
-  dragonGreen2 = "8a9a7b";
-  dragonAqua = "8ea4a2";
-  dragonBlue = "8ba4b0";
-  dragonViolet = "8992a7";
-  dragonPink = "a292a3";
-  dragonTeal = "949fb5";
+  inherit
+    (config.lib.stylix.colors)
+    base00
+    base01
+    base02
+    base03
+    base04
+    base05
+    base06
+    base07
+    base08
+    base09
+    base0A
+    base0B
+    base0C
+    base0D
+    base0E
+    base0F
+    ;
 in {
   programs.btop = {
     enable = true;
@@ -33,12 +36,11 @@ in {
       io_mode = true;
       only_physical = true;
       proc_tree = true;
-      rounded_corners = true;
+      rounded_corners = false;
       show_coretemp = true;
       show_disks = true;
       show_gpu_info = "on";
       show_uptime = true;
-      theme_background = false;
       update_ms = 500;
       truecolor = true;
       graph_symbol = "braille";
@@ -47,91 +49,83 @@ in {
   };
 
   xdg.configFile."btop/themes/kanagawa-dragon.theme".text = ''
-    # Bashtop Kanagawa-dragon (https://github.com/rebelot/kanagawa.nvim) theme
-    # By: Marcus441
+    # Bashtop Kanagawa-dragon (Base16 Refactored)
+    # Ported to Base16 Stylix scheme
 
     # Main bg
-    theme[main_bg]="#${dragonBg}"
+    theme[main_bg]="#${base00}"
 
     # Main text color
-    theme[main_fg]="#${dragonFg}"
+    theme[main_fg]="#${base05}"
 
     # Title color for boxes
-    theme[title]="#${dragonFg}"
+    theme[title]="#${base05}"
 
     # Highlight color for keyboard shortcuts
-    theme[hi_fg]="#${dragonRed}"
+    theme[hi_fg]="#${base08}"
 
     # Background color of selected item in processes box
-    theme[selected_bg]="#${dragonSelBg}"
+    theme[selected_bg]="#${base02}"
 
     # Foreground color of selected item in processes box
-    theme[selected_fg]="#${dragonSelFg}"
+    theme[selected_fg]="#${base0A}"
 
     # Color of inactive/disabled text
-    theme[inactive_fg]="#${dragonGray}"
+    theme[inactive_fg]="#${base03}"
 
-    # Misc colors for processes box including mini cpu graphs, details memory graph and details status text
-    theme[proc_misc]="#${dragonAqua}"
+    # Misc colors for processes box
+    theme[proc_misc]="#${base0C}"
 
-    # Cpu box outline color
-    theme[cpu_box]="#${dragonGray}"
-
-    # Memory/disks box outline color
-    theme[mem_box]="#${dragonGray}"
-
-    # Net up/down box outline color
-    theme[net_box]="#${dragonGray}"
-
-    # Processes box outline color
-    theme[proc_box]="#${dragonGray}"
-
-    # Box divider line and small boxes line color
-    theme[div_line]="#${dragonGray}"
+    # Box outline colors
+    theme[cpu_box]="#${base03}"
+    theme[mem_box]="#${base03}"
+    theme[net_box]="#${base03}"
+    theme[proc_box]="#${base03}"
+    theme[div_line]="#${base03}"
 
     # Temperature graph colors
-    theme[temp_start]="#${dragonGreen2}"
-    theme[temp_mid]="#${dragonOrange}"
-    theme[temp_end]="#${dragonRed}"
+    theme[temp_start]="#${base0B}"
+    theme[temp_mid]="#${base09}"
+    theme[temp_end]="#${base08}"
 
     # CPU graph colors
-    theme[cpu_start]="#${dragonGreen}"
-    theme[cpu_mid]="#${dragonYellow}"
-    theme[cpu_end]="#${dragonRed}"
+    theme[cpu_start]="#${base0B}"
+    theme[cpu_mid]="#${base0A}"
+    theme[cpu_end]="#${base08}"
 
     # Mem/Disk free meter
-    theme[free_start]="#${dragonRed}"
-    theme[free_mid]="#${dragonPink}"
-    theme[free_end]="#${dragonRed}"
+    theme[free_start]="#${base08}"
+    theme[free_mid]="#${base0E}"
+    theme[free_end]="#${base08}"
 
     # Mem/Disk cached meter
-    theme[cached_start]="#${dragonGreen2}"
-    theme[cached_mid]="#${dragonOrange}"
-    theme[cached_end]="#${dragonOrange}"
+    theme[cached_start]="#${base0B}"
+    theme[cached_mid]="#${base09}"
+    theme[cached_end]="#${base09}"
 
     # Mem/Disk available meter
-    theme[available_start]="#${dragonViolet}"
-    theme[available_mid]="#${dragonTeal}"
-    theme[available_end]="#${dragonBlue}"
+    theme[available_start]="#${base0F}"
+    theme[available_mid]="#${base06}"
+    theme[available_end]="#${base0D}"
 
     # Mem/Disk used meter
-    theme[used_start]="#${dragonBlue}"
-    theme[used_mid]="#${dragonTeal}"
-    theme[used_end]="#${dragonAqua}"
+    theme[used_start]="#${base0D}"
+    theme[used_mid]="#${base06}"
+    theme[used_end]="#${base0C}"
 
     # Download graph colors
-    theme[download_start]="#${dragonTeal}"
-    theme[download_mid]="#${dragonViolet}"
-    theme[download_end]="#${dragonPink}"
+    theme[download_start]="#${base04}"
+    theme[download_mid]="#${base0F}"
+    theme[download_end]="#${base0E}"
 
     # Upload graph colors
-    theme[upload_start]="#${dragonOrange}"
-    theme[upload_mid]="#${dragonYellow}"
-    theme[upload_end]="#${dragonRed}"
+    theme[upload_start]="#${base09}"
+    theme[upload_mid]="#${base0A}"
+    theme[upload_end]="#${base08}"
 
-    # Process box color gradient for threads, mem and cpu usage
-    theme[process_start]="#${dragonGreen}"
-    theme[process_mid]="#${dragonYellow}"
-    theme[process_end]="#${dragonRed}"
+    # Process box color gradient
+    theme[process_start]="#${base0B}"
+    theme[process_mid]="#${base0A}"
+    theme[process_end]="#${base08}"
   '';
 }
