d3329d95acf66c11ce91e75ce46db08c48d966c9
Marcus441 <psminchia4153@hotmail.com>
2026-04-10 14:32:13 +1000

    feat(ly): disable session logging for ly

--- nixos/modules/ly.nix
+++ nixos/modules/ly.nix
@@ -1,12 +1,14 @@
 {
   systemd.services.display-manager.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
-  services.displayManager.ly.enable = true;
-  services.displayManager.ly.settings = {
-    bigclock = "en";
-    bigclock_12hr = true;
-    vi_mode = true;
-    hide_borders = true;
-    bg = "0x00181616";
-    fg = "0x007FB4CA";
+  services = {
+    displayManager.ly.enable = true;
+    displayManager.ly.settings = {
+      bigclock = "en";
+      bigclock_12hr = true;
+      vi_mode = true;
+      bg = "0x00181616";
+      fg = "0x007FB4CA";
+      session_log = null;
+    };
   };
 }
