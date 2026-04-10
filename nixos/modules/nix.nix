940c79acdcb54c01110291be1d9d6cdfd3cee127
Marcus441 <psminchia4153@hotmail.com>
2026-04-10 17:54:47 +1000

    feat(nix): add cachix substituters for danth, nvf and bump nixpkgs

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -7,12 +7,16 @@
       extra-substituters = [
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://danth.cachix.org"
+        "https://nvf.cachix.org"
         "https://walker.cachix.org"
         "https://walker-git.cachix.org"
       ];
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "danth.cachix.org-1:1ow3ZHcCp6ujnzcK/FPR0gqMMoWijV9foAPvCliY0bQ="
+        "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
       ];
