3270c7c8e72d864676acaceebe4e46375f772ff3
Marcus441 <psminchia4153@hotmail.com>
2026-04-13 19:44:08 +1000

    feat(hosts): added router login host

--- nixos/modules/net.nix
+++ nixos/modules/net.nix
@@ -2,6 +2,9 @@
   # Faster boot time, does not wait to connect to internet on boot
   systemd.services.NetworkManager-wait-online.enable = false;
   networking = {
+    hosts = {
+      "192.168.1.1" = ["routerlogin.net" "www.routerlogin.net"];
+    };
     networkmanager = {
       enable = true;
       wifi = {
