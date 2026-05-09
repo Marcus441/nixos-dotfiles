d599c4f6a44ed217726a03e62f08dea6b869d524
Marcus441 <88486170+Marcus441@users.noreply.github.com>
2026-05-09 14:45:45 +1000

    feat(neovim): implement dual ide/editor setup

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -2,20 +2,44 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  inherit (pkgs.stdenv.hostPlatform) system;
+  neovim = inputs.neovim-config.packages.${system};
+in {
   home = {
     packages = [
-      inputs.neovim-config.packages.${pkgs.stdenv.hostPlatform.system}.default
+      neovim.min
     ];
-
     sessionVariables = {
       EDITOR = "nvim";
       VISUAL = "nvim";
     };
-
     shellAliases = {
       vi = "nvim";
       vim = "nvim";
+    };
+  };
+
+  programs.neovide = {
+    enable = true;
+    settings = {
+      neovim-bin = "${neovim.gui}/bin/nvim";
+      fork = false;
+      frame = "full";
+      idle = true;
+      maximized = false;
+      no-multigrid = false;
+      srgb = false;
+      tabs = true;
+      theme = "dark";
+      title-hidden = true;
+      vsync = true;
+      wsl = false;
+
+      font = {
+        normal = ["IosevkaTerm Nerd Font Mono"];
+        size = 20.0;
+      };
     };
   };
 }
